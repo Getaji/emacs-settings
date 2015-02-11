e5f42f12bbd80ea846de0dab455451fa340f420f
Margherita <margherita@localhost.localdomain>
2015-02-12 02:10:35 +0900

    The separation of the smooth-scroll Perhaps this is the last of the separation.

--- init.el
+++ init.el
@@ -1,16 +1,9 @@
-; Package ======================================================================
 (require 'cask "~/.cask/cask.el")
 (cask-initialize)
 (require 'pallet)
 (pallet-mode t)
 
 (require 'use-package)
-
-;; scroll
-(use-package smooth-scroll
-  :config
-  (smooth-scroll-mode t))
-
 
 (use-package init-loader
   :config
---
+++ inits/40-smooth-scroll.el
@@ -0,0 +1,3 @@
+(use-package smooth-scroll
+  :config
+  (smooth-scroll-mode t))
