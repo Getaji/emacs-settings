4ca3221d2023f64b2f3840c1dd4595b2fa55d4d5
Margherita <margherita@localhost.localdomain>
2015-02-12 02:04:38 +0900

    The separation of the packages

--- init.el
+++ init.el
@@ -4,25 +4,7 @@
 (require 'pallet)
 (pallet-mode t)
 
-(add-to-list 'load-path "%APPDATA%/.emacs.d/emacs-async")
-(add-to-list 'load-path "%APPDATA%/.emacs.d/helm")
-
-
 (require 'use-package)
-
-(use-package package
-  :config
-  (add-to-list 'package-archives
-               '("melpa" . "http://melpa.org/packages/") t)
-  (add-to-list 'package-archives '
-               ("marmalade" . "http://marmalade-repo.org/packages/"))
-  (when (< emacs-major-version 24)
-    (add-to-list 'package-archives
-                 '("gnu" . "http://elpa.gnu.org/packages/")))
-  (package-initialize))
-
-
-; Cask
 
 ; auto-complete =============================================================
 (use-package auto-complete
---
+++ inits/10-packages.el
@@ -0,0 +1,13 @@
+(add-to-list 'load-path "%APPDATA%/.emacs.d/emacs-async")
+(add-to-list 'load-path "%APPDATA%/.emacs.d/helm")
+
+(use-package package
+  :config
+  (add-to-list 'package-archives
+               '("melpa" . "http://melpa.org/packages/") t)
+  (add-to-list 'package-archives '
+               ("marmalade" . "http://marmalade-repo.org/packages/"))
+  (when (< emacs-major-version 24)
+    (add-to-list 'package-archives
+                 '("gnu" . "http://elpa.gnu.org/packages/")))
+  (package-initialize))
