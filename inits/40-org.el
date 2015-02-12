b87520653ec000380da8691d70da914660dfee59
Margherita <margherita@localhost.localdomain>
2015-02-12 15:38:45 +0900

    Add todo keywords and faces to org-mode

--- inits/40-org.el
+++ inits/40-org.el
@@ -2,4 +2,10 @@
   :config
   (setq org-directory "~/Documents/junk")
   (setq org-agenda-files (list org-directory))
-  (setq org-src-fontify-natively t))
+  (setq org-src-fontify-natively t)
+  (setq org-todo-keywords '((type "TODO" "FREEZE" "DOING" "CANCELLED" "DONE")))
+  (setq org-todo-keyword-faces
+        '(("TODO" :foreground "red" :weight bold)
+          ("FREEZE" :foreground "skyblue" :weight bold)
+          ("DOING" :foreground "orange" :weight bold)
+          ("CANCELLED" :foreground "cyan" :weight bold))))
