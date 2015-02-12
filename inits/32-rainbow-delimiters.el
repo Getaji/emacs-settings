d518e479d8bf45997627901abf4cfb5e620d4492
Getaji <t.tanaka.onion@gmail.com>
2015-02-12 23:39:44 +0900

    rainbow-delimiters darker of the color

--- inits/32-rainbow-delimiters.el
+++ inits/32-rainbow-delimiters.el
@@ -1,3 +1,12 @@
+(require 'cl-lib)
+(require 'color)
 (use-package rainbow-delimiters
   :config
-  (add-hook 'scheme-mode-hook 'rainbow-delimiters-mode))
+  (add-hook 'scheme-mode-hook 'rainbow-delimiters-mode)
+  (add-hook 'emacs-lisp-mode-hook 'rainbow-delimiters-mode)
+  (cl-loop
+   for index from 1 to rainbow-delimiters-max-face-count
+   do
+   (let ((face (intern (format "rainbow-delimiters-depth-%d-face"
+                               index))))
+     (cl-callf color-saturate-name (face-foreground face) 30))))
