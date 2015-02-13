584c5146c3266a2a5f7ff89f7360355a5c3a1b63
Getaji <t.tanaka.onion@gmail.com>
2015-02-13 14:39:34 +0900

    Add recentf-ext settings * Increased display number and save number

--- inits/40-recentf-ext.el
+++ inits/40-recentf-ext.el
@@ -1,4 +1,6 @@
 (use-package recentf-ext
   :bind (("C-x C-r" . recentf-open-files))
   :config
-  (setq inhibit-startup-message t))
+  (setq inhibit-startup-message t)
+  (setq recentf-max-saved-items 10)
+  (setq recentf-max-menu-items 10))
